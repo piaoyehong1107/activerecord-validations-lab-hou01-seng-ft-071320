dfb563e1469d4c11c6718c125df4f0b0548adfe2
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-19 04:41:17 +0000

    Automatically backed up by Learn

---
+++ app/validators/clickbait_validator.rb
@@ -0,0 +1,11 @@
+class ClickbaitValidator < ActiveModel::Validator 
+  def validate(record) 
+    test=["Won't Believe", "Secret", "Top [number]","Guess"]
+    test.each { |index|
+    if record.title[index]
+       return
+     end
+      record.errors[:clickbait]<< "not valid"
+    }
+  end
+end
