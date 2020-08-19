03cf1fa50edd7b2a3a49ba40e602e912993233e2
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-19 04:48:18 +0000

    Automatically backed up by Learn

--- app/validators/clickbait_validator.rb
+++ app/validators/clickbait_validator.rb
@@ -2,7 +2,7 @@ class ClickbaitValidator < ActiveModel::Validator
   def validate(record) 
     test=["Won't Believe", "Secret", "Top [number]","Guess"]
     test.each { |index|
-    if record.title[index]
+    if record.title.include?(index)
        return
      end
       record.errors[:clickbait]<< "not valid"
