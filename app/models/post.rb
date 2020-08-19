735fce54ed6cc6df7239a1fdcf0e2f3e1ebf8b11
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-19 04:45:56 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
   
-  if title.valid?
+  if valid?
     include ActiveModel::Validations
     validates_with ClickbaitValidator
   end
