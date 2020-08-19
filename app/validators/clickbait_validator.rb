a983caa6841d9db0ee86333ff64189f6ec27f7b2
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-19 04:51:01 +0000

    Done.

---
+++ app/validators/clickbait_validator.rb
@@ -0,0 +1,13 @@
+class ClickbaitValidator < ActiveModel::Validator 
+  def validate(record) 
+    test=["Won't Believe", "Secret", "Top [number]","Guess"]
+    test.each { |index|
+    if record.title.blank?
+      record.errors[:title]
+    elsif record.title.include?(index)
+       return
+    end
+      record.errors[:clickbait]<< "not valid"
+    }
+  end
+end
